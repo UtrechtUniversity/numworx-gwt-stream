7c656a0aa9d83f5f34fb4c0e6c9f800db981cac8
Frank Gerlings <f.gerlings@student.ru.nl>
2021-08-03 14:26:43 +0200

    changed fonts to more common monospace

--- src/Tree/Draw.elm
+++ src/Tree/Draw.elm
@@ -187,7 +187,7 @@ multilineEditableTextBox id nodeType content minBoxWidth minBoxHeight maxBoxWidt
                 , css
                     [ overflow auto
                     , resize Css.none
-                    , fontFamilies [ "monaco", "monofur", "monospace" ]
+                    , fontFamilies [ "lucida console", "monaco", "courier", "monospace" ]
                     , backgroundColor (Css.rgba 0 0 0 0)
                     , borderColor (Css.rgba 0 0 0 0)
                     , textAligning
